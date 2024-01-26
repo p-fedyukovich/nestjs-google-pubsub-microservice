6cf4934027706edfa84eb32c79e3a9eedde26e61
Ang Puo Syer <ang@tawk.to>
2024-01-26 13:18:42 +0800

    fix: 🐛 (interface) seperate client and server interface

--- lib/gc-pubsub.interface.ts
+++ lib/gc-pubsub.interface.ts
@@ -3,11 +3,10 @@ import { Deserializer, Serializer } from '@nestjs/microservices';
 import { PublishOptions } from '@google-cloud/pubsub/build/src/publisher';
 import { SubscriberOptions } from '@google-cloud/pubsub/build/src/subscriber';
 
-export interface GCPubSubOptions {
+export interface GCPubSubClientOptions {
   client?: ClientConfig;
   topic?: string;
   replyTopic?: string;
-  subscription?: string;
   replySubscription?: string;
   noAck?: boolean;
   init?: boolean;
@@ -21,5 +20,20 @@ export interface GCPubSubOptions {
   autoDeleteSubscriptionOnShutdown?: boolean;
   clientIdFilter?: boolean;
   appendClientIdToSubscription?: boolean;
+}
+
+export interface GCPubSubServerOptions {
+  client?: ClientConfig;
+  topic?: string;
+  subscription?: string;
+  noAck?: boolean;
+  init?: boolean;
+  checkExistence?: boolean;
+  publisher?: PublishOptions;
+  subscriber?: SubscriberOptions;
+  serializer?: Serializer;
+  deserializer?: Deserializer;
+  createSubscriptionOptions?: CreateSubscriptionOptions;
+  autoResume?: boolean;
   ackAfterResponse?: boolean;
 }
